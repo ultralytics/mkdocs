434414384fd071ac200357de153ccebcdbf44f86
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-05-08 20:07:58 +0200

    Plugin updates (#3)
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
 # Ultralytics MkDocs plugin
-# Usage:
-# pip install mkdocs-ultralytics-plugin
-# plugins:
-#   - mkdocstrings
-#   - search
-#   - ultralytics
 
 from setuptools import setup, find_packages
 from pathlib import Path
 
 setup(
     name='mkdocs-ultralytics-plugin',
-    version='0.0.9',
-    description='An MkDocs plugin that generates meta description and image tags based on the first paragraph and the '
-                'first image in a page',
+    version='0.0.11',
+    description='An MkDocs plugin that provides Ultralytics Docs customizations at https://docs.ultralytics.com.',
     long_description=Path('README.md').read_text(encoding='utf-8'),
+    long_description_content_type='text/markdown',
     author='Ultralytics',
     author_email='hello@ultralytics.com',
+    url='https://github.com/ultralytics/ultralytics',
+    project_urls={
+        'Bug Reports': 'https://github.com/ultralytics/mkdocs/issues',
+        'Funding': 'https://ultralytics.com',
+        'Source': 'https://github.com/ultralytics/mkdocs'},
     license='AGPL-3.0',
     packages=find_packages(),
     install_requires=[
