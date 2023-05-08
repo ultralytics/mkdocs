8a2d5f9bca84ba6acc628f459bd607463aa2ad05
Sourcery AI <>
2023-05-08 15:55:32 +0000

    'Refactored by Sourcery'

--- plugin.py
+++ plugin.py
@@ -7,15 +7,11 @@ class DescriptionImagePlugin(BasePlugin):
     def on_page_content(self, content, page, config, files):
         soup = BeautifulSoup(content, 'html.parser')
 
-        # Extract the first paragraph text
-        first_paragraph = soup.find('p')
-        if first_paragraph:
+        if first_paragraph := soup.find('p'):
             meta_description = first_paragraph.text.strip()
             page.meta['description'] = meta_description
 
-        # Extract the first image URL
-        first_image = soup.find('img')
-        if first_image:
+        if first_image := soup.find('img'):
             meta_image = first_image['src']
             page.meta['image'] = meta_image
 
@@ -27,23 +23,18 @@ class DescriptionImagePlugin(BasePlugin):
 
         # Update meta description
         soup = BeautifulSoup(output, 'html.parser')
-        if 'description' in page.meta and (10 < len(page.meta['description']) < 500):
-            meta_description = soup.find("meta", attrs={"name": "description"})
-
-            if meta_description:
+        if meta_description := soup.find("meta", attrs={"name": "description"}):
+            if 'description' in page.meta and (10 < len(page.meta['description']) < 500):
                 print(f'START_DESCRIPTION\n{meta_description}\nEND_DESCRIPTION\n\n')
                 meta_description['content'] = page.meta['description']
 
         # Update meta image
         if 'image' in page.meta:
-            meta_image = soup.find("meta", attrs={"property": "og:image"})
+            if meta_image := soup.find("meta", attrs={"property": "og:image"}):
+                meta_image['content'] = page.meta['image']
 
-            # If the meta image tag is not found, create and add it
-            if not meta_image:
+            else:
                 meta_image = soup.new_tag("meta")
                 meta_image.attrs.update({'property': 'og:image', 'content': page.meta['image']})
                 soup.head.append(meta_image)
-            else:
-                meta_image['content'] = page.meta['image']
-
         return str(soup)
