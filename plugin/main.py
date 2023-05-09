91f4ac7341223a811fe71179b283bb49af36f7f6
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-05-09 19:23:52 +0200

    Add verbose, enabled args

--- plugin/main.py
+++ plugin/main.py
@@ -4,12 +4,13 @@ from mkdocs.config import config_options
 
 
 class MetaPlugin(BasePlugin):
-    
     # Plugin arguments
     config_scheme = (
         ('verbose', config_options.Type(bool, default=True)),
         ('enabled', config_options.Type(bool, default=True)),
         ('default_image', config_options.Type(str, default=None)),
+        ('add_desc', config_options.Type(bool, default=True)),
+        ('add_image', config_options.Type(bool, default=True)),
     )
 
     def on_page_content(self, content, page, config, files):
@@ -19,16 +20,17 @@ class MetaPlugin(BasePlugin):
         soup = BeautifulSoup(content, 'html.parser')
 
         # Check if custom description is already defined in the Markdown header
-        if 'description' not in page.meta:
+        if self.config['add_desc'] and 'description' not in page.meta:
             if first_paragraph := soup.find('p'):
                 meta_description = first_paragraph.text.strip()
                 page.meta['description'] = meta_description
 
-        if first_image := soup.find('img'):
-            meta_image = first_image['src']
-            page.meta['image'] = meta_image
-        elif self.config['default_image']:
-            page.meta['image'] = self.config['default_image']
+        if self.config['add_image']:
+            if first_image := soup.find('img'):
+                meta_image = first_image['src']
+                page.meta['image'] = meta_image
+            elif self.config['default_image']:
+                page.meta['image'] = self.config['default_image']
 
         return content
 
@@ -42,13 +44,13 @@ class MetaPlugin(BasePlugin):
         # Update meta description
         soup = BeautifulSoup(output, 'html.parser')
         if meta_description := soup.find("meta", attrs={"name": "description"}):
-            if 'description' in page.meta and (10 < len(page.meta['description']) < 500):
+            if self.config['add_desc'] and 'description' in page.meta and (10 < len(page.meta['description']) < 500):
                 if self.config['verbose']:
                     print(f'File: {page.file.src_path}, Description: {page.meta["description"]}')
                 meta_description['content'] = page.meta['description']
 
         # Update meta image
-        if 'image' in page.meta:
+        if self.config['add_image'] and 'image' in page.meta:
             if meta_image := soup.find("meta", attrs={"property": "og:image"}):
                 meta_image['content'] = page.meta['image']
 
