a08ca42af1f7c7aa79b154ec60a73198387a7c25
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-05-09 03:19:12 +0200

    Add arguments to plugin (#4)
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- plugin/main.py
+++ plugin/main.py
@@ -1,23 +1,41 @@
 from bs4 import BeautifulSoup
 from mkdocs.plugins import BasePlugin
+from mkdocs.config import config_options
 
 
 class MetaPlugin(BasePlugin):
+    
+    # Plugin arguments
+    config_scheme = (
+        ('verbose', config_options.Type(bool, default=True)),
+        ('enabled', config_options.Type(bool, default=True)),
+        ('default_image', config_options.Type(str, default=None)),
+    )
 
     def on_page_content(self, content, page, config, files):
+        if not self.config['enabled']:
+            return content
+
         soup = BeautifulSoup(content, 'html.parser')
 
-        if first_paragraph := soup.find('p'):
-            meta_description = first_paragraph.text.strip()
-            page.meta['description'] = meta_description
+        # Check if custom description is already defined in the Markdown header
+        if 'description' not in page.meta:
+            if first_paragraph := soup.find('p'):
+                meta_description = first_paragraph.text.strip()
+                page.meta['description'] = meta_description
 
         if first_image := soup.find('img'):
             meta_image = first_image['src']
             page.meta['image'] = meta_image
+        elif self.config['default_image']:
+            page.meta['image'] = self.config['default_image']
 
         return content
 
     def on_post_page(self, output, page, config):
+        if not self.config['enabled']:
+            return output
+
         if 'description' not in page.meta and 'image' not in page.meta:
             return output
 
@@ -25,7 +43,8 @@ class MetaPlugin(BasePlugin):
         soup = BeautifulSoup(output, 'html.parser')
         if meta_description := soup.find("meta", attrs={"name": "description"}):
             if 'description' in page.meta and (10 < len(page.meta['description']) < 500):
-                print(f'START_DESCRIPTION\n{meta_description}\nEND_DESCRIPTION\n\n')
+                if self.config['verbose']:
+                    print(f'File: {page.file.src_path}, Description: {page.meta["description"]}')
                 meta_description['content'] = page.meta['description']
 
         # Update meta image
