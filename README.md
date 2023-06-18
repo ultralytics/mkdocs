8753e00a7997f51513b33ec03ccac190f121fa1a
Glenn Jocher <glenn.jocher@ultralytics.com>
2023-06-18 14:04:12 +0200

    Add keywords and additional features (#7)

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # MkDocs Ultralytics Plugin
 
-The MkDocs Ultralytics Plugin is an easy-to-use plugin that generates meta description and image tags based on the first paragraph and the first image in a page. It's specifically designed for MkDocs and provides a seamless integration to enhance your documentation.
+The MkDocs Ultralytics Plugin is an easy-to-use plugin that enhances your MkDocs site's SEO and user engagement by generating meta tags (description, image, keywords) and adding interactive features based on your Markdown content.
+
+## Features
+
+- Automatically generates meta description and image tags based on the first paragraph and the first image of a page, respectively.
+- Allows manual specification of meta keywords in the Markdown frontmatter.
+- Generates Open Graph (Facebook) and Twitter meta tags for better social media sharing.
+- Adds social share buttons for Twitter and LinkedIn at the end of each page.
+- Fetches git information (dates and authors) from your repository and appends it to the page footer.
 
 ## Installation
 
@@ -21,17 +29,61 @@ plugins:
   - ultralytics
 ```
 
+## Plugin Arguments
+
+The plugin supports the following arguments:
+
+- `verbose`: Enable or disable verbose output (default: `True`)
+- `enabled`: Enable or disable the plugin (default: `True`)
+- `default_image`: Set a default image URL if no image is found in the content (default: `None`)
+- `add_desc`: Enable or disable the generation of meta description tags (default: `True`)
+- `add_image`: Enable or disable the generation of meta image tags (default: `True`)
+- `add_share_buttons`: Enable or disable the addition of share buttons for Twitter and LinkedIn (default: `True`)
+- `add_dates`: Enable or disable the addition of git dates to the page footer (default: `True`)
+- `add_authors`: Enable or disable the addition of git authors to the page footer (default: `True`)
+
+To use the arguments, add them to the `ultralytics` plugin section in your `mkdocs.yml` file:
+
+```yaml
+plugins:
+  - mkdocstrings
+  - search
+  - ultralytics:
+      verbose: True
+      enabled: True
+      default_image: "https://example.com/default-image.png"
+      add_desc: True
+      add_image: True
+      add_share_buttons: True
+      add_dates: True
+      add_authors: True
+```
+
 ## How it works
 
-The plugin works by extracting the first paragraph and the first image (if available) from your Markdown content. It then generates and adds the appropriate meta description and image tags to the page's `<head>` section.
+The plugin works by processing your Markdown content, extracting relevant information, and generating additional meta tags and interactive features.
 
 ### Meta Description
 
-The meta description is extracted from the first paragraph of your Markdown content. The generated description is then added to the page's `<head>` section as a `<meta name="description">` tag.
+If `add_desc` is enabled, the meta description is extracted from the first paragraph of your Markdown content. The generated description is then added to the page's `<head>` section as a `<meta name="description">` tag.
 
 ### Meta Image
 
-The meta image is extracted from the first image (if available) in your Markdown content. The generated image URL is then added to the page's `<head>` section as a `<meta property="og:image">` tag.
+If `add_image` is enabled, the meta image is extracted from the first image in your Markdown content. If no image is available, it uses the `default_image` (if specified). The image URL is added as `<meta property="og:image">` and `<meta property="twitter:image">` tags.
+
+### Meta Keywords
+
+You can manually specify meta keywords in the Markdown frontmatter. These will be added as a `<meta name="keywords">` tag in the page's `<head>` section.
+
+### Share Buttons
+
+If `add_share_buttons` is enabled, share buttons for Twitter and LinkedIn are added to the page. These allow users to easily share your content on social media.
+
+### Git Dates and Authors
+
+If `add_dates` or `add_authors` is enabled,
+
+the plugin fetches relevant git information from your repository and appends it to the page footer. This keeps your readers informed about the recency and authorship of the content.
 
 ## Plugin Code
 
@@ -41,14 +93,14 @@ The core functionality of the plugin is implemented in `plugin.py`, which define
 from bs4 import BeautifulSoup
 from mkdocs.plugins import BasePlugin
 
+
 class MetaPlugin(BasePlugin):
 
     def on_page_content(self, content, page, config, files):
-        # ... (code to generate meta description and image)
+    # ... (code to generate meta description and image)
 
     def on_post_page(self, output, page, config):
-        # ... (code to update the output with the generated meta tags)
-
+    # ... (code to update the output with the generated meta tags)
 ```
 
 ## License
--- plugin/main.py
+++ plugin/main.py
@@ -1,6 +1,9 @@
+import os
+import subprocess
+
 from bs4 import BeautifulSoup
-from mkdocs.plugins import BasePlugin
 from mkdocs.config import config_options
+from mkdocs.plugins import BasePlugin
 
 
 class MetaPlugin(BasePlugin):
@@ -11,8 +14,37 @@ class MetaPlugin(BasePlugin):
         ('default_image', config_options.Type(str, default=None)),
         ('add_desc', config_options.Type(bool, default=True)),
         ('add_image', config_options.Type(bool, default=True)),
+        ('add_keywords', config_options.Type(bool, default=True)),  # Add new argument for keywords
         ('add_share_buttons', config_options.Type(bool, default=True)),  # Add new argument
+        ('add_dates', config_options.Type(bool, default=True)),  # Add dates section
+        ('add_authors', config_options.Type(bool, default=True)),  # Add authors section
     )
+
+    @staticmethod
+    def get_git_info(file_path):
+        os.chdir(os.path.dirname(file_path))
+
+        # Get the creation date
+        args = ['git', 'log', '--reverse', '--pretty=format:%ai', os.path.basename(file_path)]
+        creation_date = subprocess.check_output(args).decode('utf-8').split('\n')[0]
+        git_info = {'creation_date': creation_date}
+        # Get the last modification date
+        last_modification_date = subprocess.check_output(
+            ['git', 'log', '-1', '--pretty=format:%ai', os.path.basename(file_path)]).decode('utf-8')
+        git_info['last_modification_date'] = last_modification_date
+
+        # Get the authors and their contributions count
+        authors = subprocess.check_output(['git', 'log', '--pretty=format:%an', os.path.basename(file_path)]).decode(
+            'utf-8').split('\n')
+        authors_count = {}
+        for author in authors:
+            if author not in authors_count:
+                authors_count[author] = 1
+            else:
+                authors_count[author] += 1
+        git_info['authors'] = sorted(authors_count.items(), key=lambda x: x[1], reverse=True)
+
+        return git_info
 
     def on_page_content(self, content, page, config, files):
         if not self.config['enabled']:
@@ -35,7 +67,16 @@ class MetaPlugin(BasePlugin):
 
         return content
 
+    @staticmethod
+    def insert_content(soup, content_to_insert):
+        if comments_header := soup.find('h2', id='__comments'):
+            comments_header.insert_before(content_to_insert)
+        # Fallback: append the content to the md-typeset div if the comments header is not found
+        if md_typeset := soup.select_one('.md-typeset'):
+            md_typeset.append(content_to_insert)
+
     def on_post_page(self, output, page, config):
+        page_url = config['site_url'] + page.url.rstrip('/')
         if not self.config['enabled']:
             return output
 
@@ -44,41 +85,127 @@ class MetaPlugin(BasePlugin):
 
         # Update meta description
         soup = BeautifulSoup(output, 'html.parser')
+
+        # Primary Meta Tags
+        title_tag = soup.new_tag("meta")
+        title_tag.attrs.update({'name': 'title', 'content': page.title})
+        soup.head.append(title_tag)
+
+        # New block for keywords
+        if self.config['add_keywords'] and 'keywords' in page.meta:
+            meta_keywords = soup.new_tag("meta")
+            meta_keywords.attrs.update({'name': 'keywords', 'content': page.meta['keywords']})
+            soup.head.append(meta_keywords)
+
         if meta_description := soup.find("meta", attrs={"name": "description"}):
             if self.config['add_desc'] and 'description' in page.meta and (10 < len(page.meta['description']) < 500):
                 if self.config['verbose']:
                     print(f'File: {page.file.src_path}, Description: {page.meta["description"]}')
                 meta_description['content'] = page.meta['description']
 
+        # Open Graph / Facebook
+        og_type_tag = soup.new_tag("meta")
+        og_type_tag.attrs.update({'property': 'og:type', 'content': 'website'})
+        soup.head.append(og_type_tag)
+
+        og_url_tag = soup.new_tag("meta")
+        og_url_tag.attrs.update({'property': 'og:url', 'content': page_url})
+        soup.head.append(og_url_tag)
+
+        og_title_tag = soup.new_tag("meta")
+        og_title_tag.attrs.update({'property': 'og:title', 'content': page.title})
+        soup.head.append(og_title_tag)
+
+        og_description_tag = soup.new_tag("meta")
+        og_description_tag.attrs.update({'property': 'og:description', 'content': page.meta.get('description', '')})
+        soup.head.append(og_description_tag)
+
         if self.config['add_image'] and 'image' in page.meta:
             if meta_image := soup.find("meta", attrs={"property": "og:image"}):
                 meta_image['content'] = page.meta['image']
-
             else:
                 meta_image = soup.new_tag("meta")
                 meta_image.attrs.update({'property': 'og:image', 'content': page.meta['image']})
                 soup.head.append(meta_image)
 
+        # Twitter
+        twitter_card_tag = soup.new_tag("meta")
+        twitter_card_tag.attrs.update({'property': 'twitter:card', 'content': 'summary_large_image'})
+        soup.head.append(twitter_card_tag)
+
+        twitter_url_tag = soup.new_tag("meta")
+        twitter_url_tag.attrs.update({'property': 'twitter:url', 'content': page_url})
+        soup.head.append(twitter_url_tag)
+
+        twitter_title_tag = soup.new_tag("meta")
+        twitter_title_tag.attrs.update({'property': 'twitter:title', 'content': page.title})
+        soup.head.append(twitter_title_tag)
+
+        twitter_description_tag = soup.new_tag("meta")
+        twitter_description_tag.attrs.update(
+            {'property': 'twitter:description', 'content': page.meta.get('description', '')})
+        soup.head.append(twitter_description_tag)
+
+        if self.config['add_image'] and 'image' in page.meta:
+            twitter_image_tag = soup.new_tag("meta")
+            twitter_image_tag.attrs.update({'property': 'twitter:image', 'content': page.meta['image']})
+            soup.head.append(twitter_image_tag)
+
+        # Add git information (dates and authors) to the footer, if enabled
+        if self.config['add_dates'] or self.config['add_authors']:
+            git_info = self.get_git_info(page.file.abs_src_path)
+            dates_and_authors_div = '<div class="git-info" style="font-size: 0.8em; text-align: right; margin-bottom: 10px;"><br>'
+
+            if self.config['add_dates']:
+                dates_and_authors_div += f"Created {git_info['creation_date'][:10]}, Updated {git_info['last_modification_date'][:10]}"
+
+            if self.config['add_authors']:
+                if self.config['add_dates']:
+                    dates_and_authors_div += '<br>'
+                authors_str = ', '.join([f"{author[0]} ({author[1]})" for author in git_info['authors']])
+                dates_and_authors_div += f"Authors: {authors_str}"
+
+            dates_and_authors_div += '</div>'
+            dates_and_authors_div = BeautifulSoup(dates_and_authors_div, 'html.parser')
+            self.insert_content(soup, dates_and_authors_div)
+
         # Add share buttons to the footer, if enabled
         if self.config['add_share_buttons']:  # Check if share buttons are enabled
-            page_url = config['site_url'].rstrip('/') + page.url
+            # page_url = 'https://docs.ultralytics.com/modes/train'
             twitter_share_link = f"https://twitter.com/intent/tweet?url={page_url}"
             linkedin_share_link = f"https://www.linkedin.com/shareArticle?url={page_url}"
 
+            # share_buttons = f'''
+            # <div class="share-buttons" style="text-align: right;">
+            #     <a href="javascript:void(0);" onclick="window.open('{twitter_share_link}', 'TwitterShare', 'width=550,height=680,menubar=no,toolbar=no'); return false;" style="margin-right: 20px;">
+            #         <i class="fa-brands fa-twitter fa-xl"></i> Tweet
+            #     </a>
+            #     <a href="javascript:void(0);" onclick="window.open('{linkedin_share_link}', 'LinkedinShare', 'width=550,height=730,menubar=no,toolbar=no'); return false;">
+            #         <i class="fa-brands fa-linkedin fa-xl"></i> Share
+            #     </a>
+            # </div>
+            # '''
+
             share_buttons = f'''
-            <div class="share-buttons" style="text-align: right;">
-                <a href="{twitter_share_link}" target="_blank" rel="noopener noreferrer" style="margin-right: 20px;">
-                    <i class="fa-brands fa-twitter fa-xl"></i> Share on Twitter
-                </a>
-                <a href="{linkedin_share_link}" target="_blank" rel="noopener noreferrer">
-                    <i class="fa-brands fa-linkedin fa-xl"></i> Share on LinkedIn
-                </a>
+            <style>
+                .share-button:hover {{
+                    filter: brightness(1.2);
+                }}
+                .share-buttons {{
+                    display: flex;
+                    justify-content: flex-end;
+                }}
+            </style>
+            <div class="share-buttons">
+                <button onclick="window.open('{twitter_share_link}', 'TwitterShare', 'width=550,height=680,menubar=no,toolbar=no'); return false;" class="share-button" style="background-color: #1da1f2; color: white; padding: 5px 10px; border-radius: 5px; margin-right: 10px; cursor: pointer; display: flex; align-items: center;">
+                    <i class="fa-brands fa-twitter" style="margin-right: 5px;"></i> Tweet
+                </button>
+                <button onclick="window.open('{linkedin_share_link}', 'LinkedinShare', 'width=550,height=730,menubar=no,toolbar=no'); return false;" class="share-button" style="background-color: #0077b5; color: white; padding: 5px 10px; border-radius: 5px; cursor: pointer; display: flex; align-items: center;">
+                    <i class="fa-brands fa-linkedin" style="margin-right: 5px;"></i> Share
+                </button>
             </div>
             '''
-
-            if md_source_file_div := soup.select_one('.md-source-file'):
-                md_source_file_div.insert_before(BeautifulSoup(share_buttons, 'html.parser'))
-            elif md_typeset := soup.select_one('.md-typeset'):
-                md_typeset.append(BeautifulSoup(share_buttons, 'html.parser'))
+            share_buttons = BeautifulSoup(share_buttons, 'html.parser')
+            self.insert_content(soup, share_buttons)
 
         return str(soup)
