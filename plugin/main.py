e20d97ab8570712e13c1de948a4db18b5399854c
Glenn Jocher <glenn.jocher@ultralytics.com>
2024-08-14 02:37:54 +0800

    Update Twitter to X icon (#78)

--- plugin/main.py
+++ plugin/main.py
@@ -384,10 +384,10 @@ class MetaPlugin(BasePlugin):
 
             share_buttons = f"""<div class="share-buttons">
     <button onclick="window.open('{twitter_share_link}', 'TwitterShare', 'width=550,height=680,menubar=no,toolbar=no'); return false;" class="share-button hover-item">
-        <i class="fa-brands fa-twitter"></i> Tweet
+        <i class="fa-brands fa-x-twitter"></i> Tweet
     </button>
     <button onclick="window.open('{linkedin_share_link}', 'LinkedinShare', 'width=550,height=730,menubar=no,toolbar=no'); return false;" class="share-button hover-item linkedin">
-        <i class="fa-brands fa-linkedin"></i> Share
+        <i class="fa-brands fa-linkedin-in"></i> Share
     </button>
 </div>
 """
