7812b4e2b5a26028be2c2bbc437f38ebb0d75ea7
UltralyticsAssistant <web@ultralytics.com>
2024-08-30 18:06:46 +0200

    Adjust for mobile

--- plugin/utils.py
+++ plugin/utils.py
@@ -110,7 +110,7 @@ def get_github_username_from_email(email, cache, file_path="", verbose=True):
     if email.endswith("@users.noreply.github.com"):
         username = email.split("+")[-1].split("@")[0]
         avatar = f"https://github.com/{username}.png"
-        cache[email] = {'username': username, 'avatar': avatar}
+        cache[email] = {'username': username, 'avatar': requests.head(avatar, allow_redirects=True).url}
         return username, avatar
 
     # If the email is not found in the cache, query GitHub REST API
@@ -123,7 +123,7 @@ def get_github_username_from_email(email, cache, file_path="", verbose=True):
         if data["total_count"] > 0:
             username = data["items"][0]["login"]
             avatar = data["items"][0]["avatar"]
-            cache[email] = {'username': username, 'avatar': avatar}
+            cache[email] = {'username': username, 'avatar': requests.head(avatar, allow_redirects=True).url}
             return username, avatar
 
     if verbose:
