d6630ad4f5a6eb1e8c9a73674a03b0d35214116d
fatih akyon <34196005+fcakyon@users.noreply.github.com>
2025-07-08 17:40:04 +0300

    Add Copy button to Docs pages (#138)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: UltralyticsAssistant <web@ultralytics.com>
    Co-authored-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,5 +1,7 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
+__version__ = "0.1.20"
+
 from .main import MetaPlugin
 
-__all__ = ["MetaPlugin"]
+__all__ = ["MetaPlugin", "__version__"]
