095fea3f7d0e55d8a620b07d48f5ba18d7e23fb8
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-08-02 15:32:45 +0800

    Fix author profile image links (#145)
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>
    Co-authored-by: UltralyticsAssistant <web@ultralytics.com>

--- plugin/main.py
+++ plugin/main.py
@@ -417,7 +417,7 @@ class MetaPlugin(BasePlugin):
                 for author in git_info["authors"]:
                     name, url, n, avatar = author  # n is number of changes
                     div += f"""<a href="{url}" class="author-link" title="{name} ({n} change{"s" * (n > 1)})">
-    <img src="{avatar}&s=96" alt="{name}" class="hover-item" loading="lazy">
+    <img src="{avatar}{"&" if "?" in avatar else "?"}s=96" alt="{name}" class="hover-item" loading="lazy">
 </a>
 """
 
