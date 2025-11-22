8925b5d2cdd86d4d4f4c13e5d39a05f4f26852a0
Glenn Jocher <glenn.jocher@ultralytics.com>
2025-11-22 15:28:02 +0100

    Update __init__.py
    Signed-off-by: Glenn Jocher <glenn.jocher@ultralytics.com>

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -1,6 +1,6 @@
 # Ultralytics 🚀 AGPL-3.0 License - https://ultralytics.com/license
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 from .main import MetaPlugin
 from .postprocess import postprocess_site
